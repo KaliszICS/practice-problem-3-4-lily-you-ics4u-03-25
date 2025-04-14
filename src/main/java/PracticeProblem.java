db83f0e338f66a213ccd077067e2ca184e651d24
lily <lily.you@student.tdsb.on.ca>
2025-04-14 08:41:29 +0000

    completed pp 3.4

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,51 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		int sequence[] = recaman(6);
+		for(int i = 0; i < sequence.length; i++){
+			System.out.println(sequence[i]);
+		}
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static int[] recaman(int n){
+		if(n <= 0){
+			return new int[]{};
+		}
+		int[] sequence = new int[n];
+
+
+		
+		recamanHelper(sequence, n);
+		
+
+		return sequence;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+	public static void recamanHelper(int[] seq, int num){
+		if(num == 1){
+			seq[num - 1] = 1;
+			return;
+		}
+		
+		recamanHelper(seq, num - 1);
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+		int num2 =  seq[num - 2] - num;
 
-	public static void q4() {
-		//Write question 4 code here
-	}
+		boolean numIncluded = false;
+		for(int j = 0; j < num; j++){
+			if(num2 == seq[j]){
+				numIncluded = true;
+			}
+		}
 
-	public static void q5() {
-		//Write question 5 code here
+		if(num2 > 0 && !numIncluded){
+			seq[num - 1] = num2;
+		}
+		else{
+			seq[num - 1] = seq[num - 2] + num;
+		}
+		
 	}
 
 }
